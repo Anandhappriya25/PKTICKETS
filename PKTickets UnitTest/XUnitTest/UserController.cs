1f9f0a3bf198579686d3ba1f2c81ac6c44e905b3
Anandhappriya25 <anandha1025priya@gmail.com>
2022-12-08 10:51:21 +0530

    Toast

---
+++ PKTickets UnitTest/XUnitTest/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Blazorise.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PKTickets.Controllers;
+using PKTickets.Interfaces;
+using PKTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKTicketsUnitTest.XUnitTest
+{
+    public class UserController
+    {
+
+        [Fact]
+        public void List_Ok()
+        {
+            //User user = new User { UserId = 3, UserName = "Vijaya", PhoneNumber = "9441009834", Location = "Vellore",EmailId="karthi56@gmail.com",Password="123456",IsActive=true };
+            User user2 = new User { UserId = 3, UserName = "Vijay", PhoneNumber = "9441004834", Location = "Vellore", EmailId = "karth56@gmail.com", Password = "123456", IsActive = true };
+            List<User> customers = new List<User>();
+            customers.Add(user2);
+            var mockservice = new Mock<IUserRepository>();
+            mockservice.Setup(x => x.GetAllUsers()).Returns(customers);
+            var controller = new UsersController(mockservice.Object);
+            var okResult = controller.List();
+            var list = okResult as OkObjectResult;
+            var lists = list.Value as List<User>;
+            Assert.IsType<OkObjectResult>(okResult);
+            Assert.Equal(customers, lists);
+            lists.Count().CompareTo(1);
+        }
+        [Fact]
+        public void GetById_ok()
+        {
+            User user = new User { UserId = 3, UserName = "Vijay", PhoneNumber = "9441004834", Location = "Vellore", EmailId = "karth56@gmail.com", Password = "123456", IsActive = true }; 
+            var mockservice = new Mock<IUserRepository>();
+            mockservice.Setup(x => x.UserById(It.IsAny<int>())).Returns(user);
+            var controller = new UsersController(mockservice.Object);
+            var okResult = controller.GetById(3);
+            var list = okResult as OkObjectResult;
+            var result = list.Value as User;
+            Assert.IsType<OkObjectResult>(okResult);
+            Assert.Equal(user, result);
+            Assert.NotNull(result);
+            Assert.StrictEqual(3, result.UserId);
+            Assert.True(result.IsActive);
+        }
+        [Fact]
+        public void GetById_Null()
+        {
+            User user = null;
+            var mockservice = new Mock<IUserRepository>();
+            mockservice.Setup(x => x.UserById(It.IsAny<int>())).Returns(user);
+            var controller = new UsersController(mockservice.Object);
+            var result = controller.GetById(3);
+            var check = result as StatusCodeResult;
+            Assert.IsType<NotFoundResult>(result);
+            Assert.Equal(404, check.StatusCode);
+        }
+    }
+}
--- PKTickets/wwwroot/js/CreateUsers.js
+++ PKTickets/wwwroot/js/CreateUsers.js
@@ -8,9 +8,36 @@ $(document).ready(function () {
             data: $("#CreateUsers").serialize(),
             dataType: 'json',
             success: function (response) {
-                alert(response.message);
                 if (response.success == true) {
-                    setTimeout(function () { window.location = '/Home/UserList'; }, 500);
+                    $.toast({
+                        heading: 'Success',
+                        text: response.message,
+                        showHideTransition: 'slide',
+                        icon: 'success',
+                        hideAfter: 5000,
+                        position: 'top-center',
+                        stack: false,
+                        loader: false, 
+                        textAlign: 'center',
+                        bgColor: 'green',
+                        textColor: 'white'
+                    })
+                    setTimeout(function () { window.location = '/Home/UserList'; }, 4000);
+                }
+                else {
+                    $.toast({
+                        heading: 'Error',
+                        loader: false, 
+                        text: response.message,
+                        showHideTransition: 'slide',
+                        icon: 'error',
+                        hideAfter: 5000,
+                        position: 'top-center',
+                        stack: false,
+                        textAlign: 'center',
+                        bgColor: 'red',
+                        textColor: 'white'
+                    })
                 }
             },
             error: function () {
