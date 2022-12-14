cb57f90de752654fbd6c410f09a36aa1055d458f
Karthik1042000 <karthiknewsmart0@gmail.com>
2022-12-14 13:58:22 +0530

    Toast reduce

--- PKTickets/wwwroot/js/deleteTicket.js
+++ PKTickets/wwwroot/js/deleteTicket.js
@@ -8,35 +8,11 @@
             dataType: 'json',
             success: function (response) {
                 if (response.success == true) {
-                    $.toast({
-                        heading: 'Success',
-                        text: response.message,
-                        showHideTransition: 'slide',
-                        icon: 'success',
-                        hideAfter: 5000,
-                        position: 'top-center',
-                        stack: false,
-                        loader: false,
-                        textAlign: 'center',
-                        bgColor: 'green',
-                        textColor: 'white'
-                    })
+                    ToastMessage('Success', response.message, 'success', 'green')
                     setTimeout(function () { window.location = '/Home/ReservationsList'; }, 5000);
                 }
                 else {
-                    $.toast({
-                        heading: 'Error',
-                        loader: false,
-                        text: response.message,
-                        showHideTransition: 'slide',
-                        icon: 'error',
-                        hideAfter: 5000,
-                        position: 'top-center',
-                        stack: false,
-                        textAlign: 'center',
-                        bgColor: 'red',
-                        textColor: 'white'
-                    })
+                    ToastMessage('Error', response.message, 'error', 'red')
                 }
             },
             error: function () {
